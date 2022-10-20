51f32cad6d45fddbc21ebd5f5d1035b91842c6d9
niksonstha <sthanikson10@gmail.com>
2022-10-20 13:00:24 +0545

    qn1 modified

--- qn1.js
+++ qn1.js
@@ -1,16 +1,19 @@
 let student = {
-    name: "Nikson Shrestha",
-    sclass: "Bachelor",
-    RollNo: "13",
-  };
-  
-  // Before deleting
-  console.log(student);
-  
-  // After deleting
-  
-  let d = delete student.RollNo;
-  console.log(student);
-  
-  console.log(Object.keys(student).length);
-  
+  name: "Nikson Shrestha",
+  sclass: "Bachelor",
+  RollNo: "13",
+};
+
+let list = Object.keys(student);
+list.forEach((element) => {
+  console.log(element);
+});
+// Before deleting
+console.log(student);
+
+// After deleting
+
+let d = delete student.RollNo;
+console.log(student);
+
+console.log(list.length);
